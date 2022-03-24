96e809ad418764525e81087541fff516efca8c3f
vieiraruben <86010862+vieiraruben@users.noreply.github.com>
2022-03-24 16:24:29 +0100

    Fixed iOS minimum deployment version

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,32 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:latlong2/latlong.dart' as LatLng;
 import 'package:location/location.dart';
+import 'package:mapview/firestoreData/firestoreConfig/firebase_options.dart';
 import 'package:mapview/firestoreData/markers_data.dart';
 import 'marker.dart';
 import 'package:geolocator/geolocator.dart' as geo;
-import'package:flutter_map_location_marker/flutter_map_location_marker.dart';
-
-
-
+import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (Platform.isIOS) {
+         await Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+         );
+           } else {
+             await Firebase.initializeApp();
+             }
+
   await getMarkers();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   // This widget is the root of your application.
   @override
@@ -29,7 +36,6 @@ class MyApp extends StatelessWidget{
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-
       home: MyHomePage(title: 'Flutter Demo Home Page!!!!'),
     );
   }
@@ -42,7 +48,7 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>{
+class _MyHomePageState extends State<MyHomePage> {
   MyMarkers markersToDisplay = MyMarkers(markers);
   late bool _serviceEnabled;
   late PermissionStatus _permissionGranted;
@@ -72,7 +78,6 @@ class _MyHomePageState extends State<MyHomePage>{
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     _getUserLocation();
@@ -85,9 +90,8 @@ class _MyHomePageState extends State<MyHomePage>{
         nePanBoundary: LatLng.LatLng(51.511976, -0.155764),
         swPanBoundary: LatLng.LatLng(51.49, -0.187530),
         plugins: [
-            LocationMarkerPlugin(), // <-- add plugin here
+          LocationMarkerPlugin(), // <-- add plugin here
         ],
-
       ),
       layers: [
         TileLayerOptions(
@@ -97,53 +101,41 @@ class _MyHomePageState extends State<MyHomePage>{
             return Text("HereWeR");
           },
         ),
-
-        OverlayImageLayerOptions(
-            overlayImages: [OverlayImage(
-                bounds: LatLngBounds(LatLng.LatLng(51.50885, -0.1684),
-                    LatLng.LatLng(51.50485, -0.175)),
-                opacity: 1,
-                imageProvider: const NetworkImage(
-                    'https://i.pinimg.com/564x/16/98/40/169840717d863e92c4c0ffc3cacd4c55.jpg'))
-            ]
-        ),
-
-        CircleLayerOptions(
-            circles:[ CircleMarker( //radius marker
-                point: LatLng.LatLng(51.50685, -0.171870),
-                color: Colors.blue.withOpacity(0),
-                borderStrokeWidth: 3.0,
-                borderColor: Colors.blue,
-                useRadiusInMeter: true,
-                radius: 220 //radius
-            )
-            ]
-        ),
-
+        OverlayImageLayerOptions(overlayImages: [
+          OverlayImage(
+              bounds: LatLngBounds(LatLng.LatLng(51.50885, -0.1684),
+                  LatLng.LatLng(51.50485, -0.175)),
+              opacity: 1,
+              imageProvider: const NetworkImage(
+                  'https://i.pinimg.com/564x/16/98/40/169840717d863e92c4c0ffc3cacd4c55.jpg'))
+        ]),
+        CircleLayerOptions(circles: [
+          CircleMarker(
+              //radius marker
+              point: LatLng.LatLng(51.50685, -0.171870),
+              color: Colors.blue.withOpacity(0),
+              borderStrokeWidth: 3.0,
+              borderColor: Colors.blue,
+              useRadiusInMeter: true,
+              radius: 220 //radius
+              )
+        ]),
         markersToDisplay.displayMarkers(),
-
         LocationMarkerLayerOptions(
-          positionStream: const LocationMarkerDataStreamFactory().geolocatorPositionStream(
+          positionStream:
+              const LocationMarkerDataStreamFactory().geolocatorPositionStream(
             stream: geo.Geolocator.getPositionStream(
-              locationSettings:  const geo.LocationSettings(
+              locationSettings: const geo.LocationSettings(
                 accuracy: geo.LocationAccuracy.high,
                 distanceFilter: 5,
               ),
             ),
-
           ),
-          marker:  const DefaultLocationMarker(
+          marker: const DefaultLocationMarker(
             color: Colors.blue,
           ),
         )
-
-
       ],
     );
   }
-
 }
-
-
-
-
