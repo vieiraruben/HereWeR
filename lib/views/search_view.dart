c42488ad7ea6b86ee52663bda33028ca4d665741
vieiraruben <86010862+vieiraruben@users.noreply.github.com>
2022-03-31 12:29:34 +0200

    Added clear search function

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -16,7 +16,6 @@ class _SearchViewState extends State<SearchView> {
   late final TextEditingController _search;
   bool silverCollapsed = false;
   double leftPadding = 20;
-  String myTitle = "";
   bool showResults = false;
 
   @override
@@ -40,16 +39,17 @@ class _SearchViewState extends State<SearchView> {
     super.dispose();
   }
 
-  searchResults(String value) {
-    if (showResults) {
-      showResults = false;
-    } else {
+  searchResults() {
+    if (_search.text.isNotEmpty) {
       showResults = true;
+    } else {
+      showResults = false;
     }
     setState(() {});
   }
 
-  static SliverPersistentHeader makeHeader(BuildContext context, String headerText) {
+  static SliverPersistentHeader makeHeader(
+      BuildContext context, String headerText) {
     return SliverPersistentHeader(
       pinned: true,
       floating: false,
@@ -57,10 +57,56 @@ class _SearchViewState extends State<SearchView> {
         minHeight: 60.0,
         maxHeight: 60.0,
         child: Container(
-          color: Theme.of(context).backgroundColor
-          ,
-            child: Padding(padding: const EdgeInsets.all(20), child:
-            Text(headerText, style: const TextStyle(fontWeight: FontWeight.bold)),)),
+            color: Theme.of(context).backgroundColor,
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Text(headerText,
+                  style: const TextStyle(fontWeight: FontWeight.bold)),
+            )),
+      ),
+    );
+  }
+
+  SliverAppBar sliverSearchBar() {
+    return SliverAppBar(
+      excludeHeaderSemantics: true,
+      automaticallyImplyLeading: true,
+      pinned: true,
+      snap: false,
+      floating: false,
+      toolbarHeight: 90,
+      expandedHeight: 240.0,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        expandedTitleScale: 1.2,
+        background: Image.asset('assets/images/artboard.png'),
+        collapseMode: CollapseMode.parallax,
+        titlePadding:
+            EdgeInsets.only(left: leftPadding, right: 20, top: 20, bottom: 20),
+        title: TextField(
+          controller: _search,
+          onSubmitted: (value) => searchResults(),
+          textInputAction: TextInputAction.search,
+          decoration: InputDecoration(
+            suffixIcon: (showResults)
+                ? IconButton(
+                    onPressed: () {
+                      _search.clear();
+                      searchResults();
+                    },
+                    icon: Icon(Icons.clear))
+                : null,
+            isCollapsed: false,
+            isDense: true,
+            filled: true,
+            fillColor: Theme.of(context).canvasColor,
+            border: const OutlineInputBorder(),
+            hintText: "Search users, events and more...",
+          ),
+          enableSuggestions: true,
+          autocorrect: true,
+          keyboardType: TextInputType.multiline,
+        ),
       ),
     );
   }
@@ -71,137 +117,79 @@ class _SearchViewState extends State<SearchView> {
       return Scaffold(
           body:
               CustomScrollView(controller: _scrollController, slivers: <Widget>[
-        SliverAppBar(
-          excludeHeaderSemantics: true,
-          automaticallyImplyLeading: true,
-          pinned: true,
-          snap: false,
-          floating: false,
-          toolbarHeight: 90,
-          expandedHeight: 240.0,
-          flexibleSpace: FlexibleSpaceBar(
-            centerTitle: true,
-            expandedTitleScale: 1.2,
-            background: Image.asset('assets/images/artboard.png'),
-            collapseMode: CollapseMode.parallax,
-            titlePadding: EdgeInsets.only(
-                left: leftPadding, right: 20, top: 20, bottom: 20),
-            title: TextField(
-              controller: _search,
-              onSubmitted: (value) => searchResults(value),
-              textInputAction: TextInputAction.search,
-              decoration: InputDecoration(
-                isCollapsed: false,
-                isDense: true,
-                filled: true,
-                fillColor: Theme.of(context).canvasColor,
-                border: const OutlineInputBorder(),
-                hintText: "Search users, events and more...",
-              ),
-              enableSuggestions: true,
-              autocorrect: true,
-              keyboardType: TextInputType.multiline,
-            ),
-          ),
-        ), makeHeader(context, "ANNOUNCEMENTS"),
-        SliverList(delegate: SliverChildListDelegate([
+        sliverSearchBar(),
+        makeHeader(context, "ANNOUNCEMENTS"),
+        SliverList(
+            delegate: SliverChildListDelegate([
           SizedBox(
-            height: 250,
-            child: Padding(
+              height: 250,
+              child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                            children: announcements.keys.map((key) {
+                          return SizedBox(
+                              width: 400,
+                              child: Row(
+                                children: [
+                                  CircleAvatar(
+                                      backgroundColor: const Color.fromARGB(
+                                          255, 228, 228, 249),
+                                      radius: 20,
+                                      child: CircleAvatar(
+                                          radius: 12,
+                                          backgroundColor: const Color.fromARGB(
+                                              255, 228, 228, 249),
+                                          foregroundImage: Image.asset(
+                                                  'assets/images/' + key)
+                                              .image)),
+                                  const SizedBox(width: 10),
+                                  Flexible(
+                                      child: Text(
+                                    announcements[key]!,
+                                    softWrap: true,
+                                  )),
+                                  const SizedBox(
+                                    height: 60,
+                                  )
+                                ],
+                              ));
+                        }).toList())
+                      ])))
+        ])),
+        makeHeader(context, "RIGHT NOW"),
+        SliverList(
+            delegate: SliverChildListDelegate([
+          SizedBox(
+              height: 700,
+              child: Padding(
                 padding: const EdgeInsets.all(20),
                 child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Column(
-                          children: announcements.keys.map((key) {
-                        return SizedBox(
-                            width: 400,
-                            child: Row(
-                              children: [
-                                CircleAvatar(
-                                    backgroundColor:
-                                        const Color.fromARGB(255, 228, 228, 249),
-                                    radius: 20,
-                                    child: CircleAvatar(
-                                        radius: 12,
-                                        backgroundColor: const Color.fromARGB(
-                                            255, 228, 228, 249),
-                                        foregroundImage:
-                                            Image.asset('assets/images/' + key)
-                                                .image)),
-                                const SizedBox(width: 10),
-                                Flexible(
-                                    child: Text(
-                                  announcements[key]!,
-                                  softWrap: true,
-                                )),
-                                const SizedBox(
-                                  height: 60,
-                                )
-                              ],
-                            ));
-                      }).toList())])))])),
-                      makeHeader(context, "RIGHT NOW"), SliverList(delegate: SliverChildListDelegate([SizedBox(
-            height: 700,
-            child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: []
-                      ),
-                      
-            ))]))]));
+                    crossAxisAlignment: CrossAxisAlignment.start, children: []),
+              ))
+        ]))
+      ]));
     } else {
       return Scaffold(
           body:
               CustomScrollView(controller: _scrollController, slivers: <Widget>[
-        SliverAppBar(
-            excludeHeaderSemantics: true,
-            automaticallyImplyLeading: true,
-            pinned: true,
-            snap: false,
-            floating: false,
-            toolbarHeight: 90,
-            expandedHeight: 240.0,
-            title: Text(myTitle),
-            // toolbarHeight: 50,
-
-            flexibleSpace: FlexibleSpaceBar(
-                centerTitle: true,
-                expandedTitleScale: 1.2,
-                background: Image.asset('assets/images/artboard.png'),
-                collapseMode: CollapseMode.parallax,
-                titlePadding: EdgeInsets.only(
-                    left: leftPadding, right: 20, top: 20, bottom: 20),
-                title: TextField(
-                  controller: _search,
-                  onSubmitted: (value) => searchResults(value),
-                  textInputAction: TextInputAction.search,
-                  decoration: InputDecoration(
-                    isCollapsed: false,
-                    isDense: true,
-                    filled: true,
-                    fillColor: Theme.of(context).canvasColor,
-                    border: const OutlineInputBorder(),
-                    hintText: "Search users, events and more...",
-                  ),
-                  enableSuggestions: true,
-                  autocorrect: true,
-                  keyboardType: TextInputType.multiline,
-                ))),
-                SliverList(delegate: SliverChildListDelegate([SizedBox(
+        sliverSearchBar(),
+        SliverList(
+            delegate: SliverChildListDelegate([
+          SizedBox(
             height: 700,
             child: Padding(
                 padding: const EdgeInsets.all(20),
-                child: SearchResultsView(searchTerm: _search.text,)
-                
-                      ),
-                      
-            )]))
+                child: SearchResultsView(
+                  searchTerm: _search.text,
+                )),
+          )
+        ]))
       ]));
     }
   }
-
 }
 
 class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
